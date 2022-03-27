ec03e9d3672eb4598f9ba1861657d819de178683
EduApps-CDG <eduardoprocopiogomez@gmail.com>
2022-03-27 17:36:01 -0300

    Updated contact

--- src/components/contact-icon.tsx
+++ src/components/contact-icon.tsx
@@ -10,8 +10,8 @@ export default class ContactIcon extends React.Component {
         <StandardIcon path={mdiGithub} href="https://github.com/EduApps-CDG"/>
         <StandardIcon path={mdiGmail} href="mailto:eduardoprocopiogomez@gmail.com"/>
         <StandardIcon path={mdiLinkedin} href="https://linkedin.com/in/eduapps"/>
-        <StandardIcon path={mdiPhone} href="tel:+5551994217935"/>
-        <StandardIcon path={mdiWhatsapp} href="https://api.whatsapp.com/send?phone=5551994217935&text=Bom%20dia%2C%20Eduardo!"/>
+        <StandardIcon path={mdiPhone} href="tel:+5551993763209"/>
+        <StandardIcon path={mdiWhatsapp} href="https://api.whatsapp.com/send?phone=5551993763209&text=Bom%20dia%2C%20Eduardo!"/>
         <StandardIcon path={mdiFacebook} href="https://www.facebook.com/eduardo.procopio.921"/>
         <StandardIcon path={mdiDiscord} href="https://discord.com/users/389292303933767690"/>
       </div>
